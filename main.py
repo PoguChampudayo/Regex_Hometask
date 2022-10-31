e35cb99c8cfdd830314bbee179d249119cc7d28b
Aleksey Gordeyev <agordeyev94@gmail.com>
2022-10-31 20:59:24 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+import re
+import csv
+
+def get_list_from_file(raw_file_name:str):
+    '''returns a list containing each line from csv'''
+    with open(raw_file_name, 'r', encoding="utf-8") as f:
+        return list(csv.reader(f, delimiter=","))
+
+def correct_full_name(last_name:str, first_name:str, middle_name:str):
+    '''recieves raw inputs and returns a list - [last name, first name, middle name]'''
+    full_name = (last_name + ' ' + first_name + ' ' + middle_name).split()
+    while len(full_name) != 3:
+        full_name.append('')
+    return full_name
+
+def get_data_to_record(raw_file_name:str):
+    '''transforms raw data from csv file to list of lists with uniformed records'''
+    contacts_list = get_list_from_file(raw_file_name)
+    result_dic = {lastname: ['' for _ in range(7)] for lastname in [line[0].split()[0] for line in contacts_list]} #creates a dict {last_name:[data]} for every record in csv
+    for raw_record in contacts_list:
+        full_name = correct_full_name(raw_record[0], raw_record[1], raw_record[2])
+        fine_record = full_name + [raw_record[3], raw_record[4], uniform_phoes(raw_record[5]), raw_record[6]]
+        for i, element in enumerate(fine_record):
+            if not result_dic[full_name[0]][i]:
+                result_dic[full_name[0]][i] = element
+    return [record for record in result_dic.values()]
+
+def uniform_phoes(raw_phone:str):
+    '''transformes phone number from csv into "+7(999)999-99-99" or "+7(999)999-99-99 доб.9999" format'''
+    phone_pattern = r'(\+7|8)?[\s\(]?[\s\(]?(\d{3})[\s\)-]?[\s-]?(\d{3})-?(\d{2})-?(\d{2})\s?\(?(доб)?.?\s?(\d{4})?\)?'
+    if 'доб' in raw_phone:
+        return re.sub(phone_pattern, r"+7(\2)\3-\4-\5 \6.\7", raw_phone)
+    else:
+        return re.sub(phone_pattern, r"+7(\2)\3-\4-\5", raw_phone)
+    
+def record_data_to_csv(data:list, fine_file_name):
+    '''records a list with data to csv file'''
+    with open(fine_file_name, "w", encoding='utf-8', newline='') as f:
+        datawriter = csv.writer(f, delimiter=',')
+        datawriter.writerows(data)
+
+def make_contacts_similar(raw_file_name:str, fine_file_name:str):
+    result = get_data_to_record(raw_file_name)
+    record_data_to_csv(result, fine_file_name)
+    
+if __name__ =='__main__':
+    make_contacts_similar("phonebook_raw.csv", "phonebook.csv")
